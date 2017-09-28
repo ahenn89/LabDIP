0dca671f36e7431df5f9a10ef515ef34f8e80e02
Ashlee <Ashlee@10.30.12.71>
2017-09-27 19:20:44 -0500

    commit taking out random number

--- src/dip/lab3/oneLiners.java
+++ src/dip/lab3/oneLiners.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dip.lab3;
+
 import java.util.ArrayList;
-import java.util.Random;
+//import java.util.Random;
 /**
  *
  * @author Ashlee
@@ -25,13 +21,13 @@ public class oneLiners implements Input{
         badOneLiners.add("I was overcharged for velcro last week. What a rip off!");
     }
      
-     public final String selectOneLiners(){
-        Random randNum = new Random();
-        int n = randNum.nextInt(badOneLiners.size());
-        String oneLiners = badOneLiners.get(n);
-        return oneLiners;
-     }
-
+//     public final String selectOneLiners(){
+//        Random randNum = new Random();
+//        int n = randNum.nextInt(badOneLiners.size());
+//        String oneLiners = badOneLiners.get(n);
+//        return oneLiners;
+//     }
+//
         @Override
        public final String readMessage() {
            String message = selectOneLiners();
