f41741134f03888b5f1f42cfae73769409ccfcd9
Ashlee <Ashlee@10.30.12.71>
2017-09-27 19:29:23 -0500

    Revert "commit taking out random number"
    This reverts commit ceb37f364e0e906911ce383ed521f8971bd3a3bb.

--- src/dip/lab3/MessageService.java
+++ src/dip/lab3/MessageService.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package dip.lab3;
 
 /**
@@ -12,7 +17,11 @@ public class MessageService {
         String message = messageInput.readMessage();
         messageOutput.messageOutput(message);
     }
-
+     
+//     public MessageService(messageInput input){
+//        setInput(input);
+//        setOutput(output);
+//    }
      
     public Input getMessageInput() {
         return messageInput;
